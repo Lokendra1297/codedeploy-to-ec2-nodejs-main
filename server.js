18e8286c5c6a480c6fed2b611041ca4258adc64f
Lokendra1297 <82502674+Lokendra1297@users.noreply.github.com>
2022-12-01 18:09:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import createServer from "http";
-import Server from "socket.io";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 // const express = require('express');
 const app = express()
